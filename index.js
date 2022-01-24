2decc9c0e5c642f1b62f86c0648be3df1611e247
drhnopifga <64163408+drhnopifga@users.noreply.github.com>
2022-01-24 20:09:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const mineflayer = require('mineflayer')
+var options = {
+  host: 'mstnw.net',
+  username: 'DeliBlitz', 
+  version: '1.12.2'
+  }
+
+var bot = mineflayer.createBot(options);
+
+bindEvents(bot);
+
+function bindEvents(bot) {
+
+    bot.on('kicked', function(reason) {
+      console.log("Kicked");
+
+      bot = mineflayer.createBot(options);
+      bindEvents(bot);
+    });
+}
